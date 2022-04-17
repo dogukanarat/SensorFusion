75786c355cd6a1b4f61987fea97379b1765de1f1
Dogukan Arat <aratdogukan@gmail.com>
2022-04-17 22:00:35 +0300

    Add new methods to the project

--- Widgets/SFSensorControlWidget/SFSensorControlWidget.cpp
+++ Widgets/SFSensorControlWidget/SFSensorControlWidget.cpp
@@ -7,9 +7,71 @@ SFSensorControlWidget::SFSensorControlWidget(QWidget *parent)
 {
     m_ui->setupUi(this);
 
+    m_ui->accelerationX->setRange(-10, 10);
+    m_ui->accelerationX->setValue(0);
+    m_ui->accelerationX->setSingleStep(1);
+
+    m_ui->accelerationY->setRange(-10, 10);
+    m_ui->accelerationY->setValue(0);
+    m_ui->accelerationY->setSingleStep(1);
+
+    m_ui->accelerationZ->setRange(-10, 10);
+    m_ui->accelerationZ->setValue(0);
+    m_ui->accelerationZ->setSingleStep(1);
+
+    onAccelerationXValueChanged(0);
+    onAccelerationYValueChanged(0);
+    onAccelerationZValueChanged(0);
+
+
+    connect(
+        m_ui->accelerationX,
+        &QSlider::valueChanged,
+        this,
+        &SFSensorControlWidget::onAccelerationXValueChanged
+    );
+
+    connect(
+        m_ui->accelerationY,
+        &QSlider::valueChanged,
+        this,
+        &SFSensorControlWidget::onAccelerationYValueChanged
+    );
+
+    connect(
+        m_ui->accelerationZ,
+        &QSlider::valueChanged,
+        this,
+        &SFSensorControlWidget::onAccelerationZValueChanged
+    );
 }
 
 SFSensorControlWidget::~SFSensorControlWidget()
 {
     delete m_ui;
+}
+
+SFSensorControlWidget::NoiseGenerator::NoiseGenerator(double variance)
+    : m_variance{variance}
+{
+}
+
+double SFSensorControlWidget::NoiseGenerator::makeNoise(double value)
+{
+    return value + qrand() % (int)(m_variance * 2) - m_variance;
+}
+
+void SFSensorControlWidget::onAccelerationXValueChanged(int value)
+{
+    m_ui->accelerationXState->setText(QString::number(value));
+}
+
+void SFSensorControlWidget::onAccelerationYValueChanged(int value)
+{
+    m_ui->accelerationYState->setText(QString::number(value));
+}
+
+void SFSensorControlWidget::onAccelerationZValueChanged(int value)
+{
+    m_ui->accelerationZState->setText(QString::number(value));
 }
--- Widgets/SFSensorControlWidget/SFSensorControlWidget/SFSensorControlWidget.h
+++ Widgets/SFSensorControlWidget/SFSensorControlWidget/SFSensorControlWidget.h
@@ -13,11 +13,27 @@ class SFSensorControlWidget : public SFBaseWidget
 {
     Q_OBJECT
 
+    class NoiseGenerator
+    {
+    public:
+        NoiseGenerator(double variance);
+        virtual ~NoiseGenerator() = default;
+
+        double makeNoise(double value);
+
+    protected:
+        double m_variance;
+    };
+
 public:
     SFSensorControlWidget(QWidget *parent = 0);
     virtual ~SFSensorControlWidget();
 
 public slots:
+    void onAccelerationXValueChanged(int value);
+    void onAccelerationYValueChanged(int value);
+    void onAccelerationZValueChanged(int value);
+    void onButtonRunClicked();
 
 private:
     Ui::SFSensorControlWidget* m_ui;
