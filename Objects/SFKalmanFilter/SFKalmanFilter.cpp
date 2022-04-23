59c87e798dd40d4aed57d913fb755b0035de5438
Dogukan Arat <aratdogukan@gmail.com>
2022-04-23 12:14:57 +0300

    Test Eigen library

--- Objects/SFKalmanFilter/SFKalmanFilter.cpp
+++ Objects/SFKalmanFilter/SFKalmanFilter.cpp
@@ -1,6 +1,17 @@
 #include "SFKalmanFilter/SFKalmanFilter.h"
+#include <Eigen/Dense>
+#include <QDebug>
+#include <iostream>
+
+using Eigen::MatrixXd;
 
 SFKalmanFilter::SFKalmanFilter(QObject* parent)
     : QObject(parent)
 {
+    MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    std::cout << m;
 }
